436c581062289dfc48eae3262a221ddd4b83dcff
Randy Davis <randy_davis@outlook.com>
2016-10-31 23:14:04 -0500

    complete Adding Pagination

--- section4/sections/search/search.ctrl.js
+++ section4/sections/search/search.ctrl.js
@@ -10,11 +10,15 @@ function SearchController (ShowService, $timeout, StoreFactory) {
     vm.searching = false;
     vm.query = query;
     vm.typeahead = typeahead;
+    vm.currentPage = 1;
+    vm.totalResults = 0;
+
 
     function query (query) {
         vm.searching = true;
-        ShowService.search(query).then(function (response) {
-            vm.results = response;
+        ShowService.search(query, vm.currentPage).then(function (response) {
+            vm.results = response.results;
+            vm.totalResults = response.total_results;
             $timeout(function () {
                 vm.searching = false;
             }, 500);
@@ -22,8 +26,8 @@ function SearchController (ShowService, $timeout, StoreFactory) {
     }
 
     function typeahead (query) {
-        return ShowService.search(query).then(function (response) {
-            return response.map(function (show) {
+        return ShowService.search(query, 1).then(function (response) {
+            return response.results.map(function (show) {
                 return show.name;
             });
         });
